8cb68d720458070f3ce7bce614261bf1a8516942
rodrigomsr2 <rodrigomsr2@gmail.com>
2021-08-18 21:46:13 -0300

    Aula 18/08: Client adaptado para trabalhar roster

--- src/main/java/br/com/mesttra/medicaldep/rest/UnavailabilityClient.java
+++ src/main/java/br/com/mesttra/medicaldep/rest/UnavailabilityClient.java
@@ -5,12 +5,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import br.com.mesttra.medicaldep.amqp.PlayerMessage;
-
 @FeignClient(name = "players", url="http://localhost:8081/")
 public interface UnavailabilityClient {
 
-	@RequestMapping(method = RequestMethod.PUT, value = "/players/{id}/indisponibilizar")
+	@RequestMapping(method = RequestMethod.PATCH, value = "/players/{id}/unavailable")
 	void sendMail(@PathVariable("id") Long playerId);
 	
 }
